42676981df792a753c1f24ebc085821d223c9495
Karthik_K <33384466+karthik1997@users.noreply.github.com>
2020-03-18 11:19:24 +0530

    Update PeerLearning.py

--- PeerLearning.py
+++ PeerLearning.py
@@ -1,32 +1,35 @@
-class peerLearning:        
-   stack = []
-   def _init_ (self,name,position):
-		self.name = name
-		self.position = position
-   def addStacks(skill):
-		stack.append(skill)
-
-   def setMentorOrLearner(self):
-		if self.position == "mentor":
-			print (self.name,"is a mentor")
-			stack.append(self.skill)
-	        else:
-			print(self.name,"is a learner")
-
-   def setAvailableTime(self)
-    if self.role=="Mentor":
-			self.time=input("enter the availiabe time")
-    return
-
-    def getMentor(self,stack,time)
-      if self.role == "Mentor":
-			for n in stack:
-				if n in self.stack and time < self.time:
-					print(n, ":", self.name)
-		return
-time = input("Enter available time: ")
-p1 = peerLearning()
-p1.addStacks()
-p1.setMentorOrLearner()
-p1.setAvailableTime()
-p1.getMentor(stack, time)
+class PeerLearning:
+    def __init__(self,name):
+        self.name = name
+    def addStacks(self):
+        print("Whether interested or expertised.")
+        status=input("Enter 'interest/expertise' : ")
+        while(status != "interest" and status != "expertise"):
+            status=input("Enter only 'interest/expertise' : ")
+        self.status=status
+    def setMentorOrLearner(self):
+        print("Whether Partcipant is Learner or Mentor.")
+        roll = input("Enter 'learner/mentor' : ")
+        while (roll != "learner" and roll != "mentor"):
+            roll = input("Enter only 'learner/mentor' : ")
+        self.roll = roll
+    def setAvailableTime(self):
+        if self.roll == "mentor":
+            print("Enter available time")
+            time = input("In hours only: ")
+            self.time=float(time)
+    def getMentor(self,stack,time):
+        if (self.roll == "mentor" and self.time >= float(time)):
+            print(self.name, "is available")
+stack=[]
+i=0
+while(input("Add member 'y/n': ") == "y" ):
+   name=input("Enter the name: ")
+   stack.append(PeerLearning(name))
+   stack[i].addStacks()
+   stack[i].setMentorOrLearner()
+   stack[i].setAvailableTime()
+   i=i+1
+time = input("Enter the time to check availability in hours: ")
+for n in range(0,i):
+    stack[n].getMentor(stack,time)
